9fed93cc14b9db89c5f493632c783cb12f628197
Bence Balogh <benceeok@gmail.com>
2022-01-07 23:23:39 +0100

    Update __init__.py

--- src/dewloosh/core/__init__.py
+++ src/dewloosh/core/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from dewloosh.core.types.library import Library
-__version__ = "0.0.dev7"
+__version__ = "0.0.dev8"
 __description__ = "Common developer utilities and base classes to support other dewloosh packages."
 
