23f944029409101989658a2ef5f8dbbe4ba51376
Bence Balogh <benceeok@gmail.com>
2022-06-03 22:03:53 +0200

    Update __init__.py

--- src/dewloosh/core/__init__.py
+++ src/dewloosh/core/__init__.py
@@ -1,42 +1,6 @@
 # -*- coding: utf-8 -*-
-from typing import Callable
-import numpy as np
-
 from .types.library import Library
 from .abc.wrap import Wrapper
 
 __version__ = "1.0.5"
 __description__ = "Common developer utilities and base classes to support other dewloosh packages."
-
-
-def squeeze_if_array(arr) : return np.squeeze(arr) if isinstance(arr, np.ndarray) else arr
-
-
-def squeeze(default=True):
-    def decorator(fnc: Callable):
-        def inner(*args, **kwargs):
-            if kwargs.get('squeeze', default):
-                res = fnc(*args, **kwargs)
-                if isinstance(res, tuple):
-                    return list(map(squeeze_if_array, res))
-                return squeeze_if_array(res)
-            else:
-                return fnc(*args, **kwargs)
-        return inner
-    return decorator
-
-
-def config(*args, **kwargs):
-    def decorator(fnc: Callable):
-        def inner(*args, **kwargs):
-            return fnc(*args, **kwargs)
-        return inner
-    return decorator
-
-
-def is_none_or_false(a):
-    if isinstance(a, bool):
-        return not a
-    elif a is None:
-        return True
-    return False
