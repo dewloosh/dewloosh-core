c814e5ac780904b01743b8dd1e9af6b3aed971e3
Bence Balogh <benceeok@gmail.com>
2022-06-07 11:38:10 +0200

    Update __init__.py

--- src/dewloosh/core/__init__.py
+++ src/dewloosh/core/__init__.py
@@ -2,5 +2,5 @@
 from .types.library import Library
 from .abc.wrap import Wrapper
 
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 __description__ = "Common developer utilities and base classes to support other dewloosh packages."
