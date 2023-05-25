25d81dc8d87fad3adadd96934ab1adf76cfd97c9
Bence Balogh <benceeok@gmail.com>
2023-05-25 15:35:53 +0200

    added testcase

---
+++ src/dewloosh/core/testing.py
@@ -0,0 +1,14 @@
+import unittest
+from typing import Callable
+
+
+class TestCase(unittest.TestCase):
+
+    def assertFailsProperly(self, exc:Exception, fnc:Callable, *args, **kwargs):
+        failed_properly = False
+        try:
+            fnc(*args, **kwargs)
+        except exc:
+            failed_properly = True
+        finally:
+            self.assertTrue(failed_properly)
